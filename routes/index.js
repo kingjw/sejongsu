0b75ce15edd0a0df5b078303d2a14010e0935f7a
Ubuntu <gkstjdwls11@naver.com>
2018-06-26 03:47:05 +0000

    성진

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/sejongsu/login', function(req, res) {
+  res.render('login', { title: 'Express' });
+});
+router.get('/sejongsu/loginP', function(req, res) {
+  res.render('loginP', { title: 'Express' });
+});
+router.get('/sejongsu/join', function(req, res) {
+  res.render('join', { title: 'Express' });
+});
+router.get('/sejongsu/joinP', function(req, res) {
+  res.render('joinP', { title: 'Express' });
+});
+
+module.exports = router;

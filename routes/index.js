e147c2ca6ee6c073b1f82f603d278b2e84935ebe
seongjin571 <gkstjdwls11@naver.com>
2018-06-26 13:01:40 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-<<<<<<< HEAD
 router.get('/sejongsu/login', function(req, res) {
   res.render('login', { title: 'Express' });
 });
@@ -14,10 +13,6 @@ router.get('/sejongsu/join', function(req, res) {
 });
 router.get('/sejongsu/joinP', function(req, res) {
   res.render('joinP', { title: 'Express' });
-=======
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
->>>>>>> ca7e29fcd9d9536b31d2d5110ca396e281cc3837
 });
 
 module.exports = router;
